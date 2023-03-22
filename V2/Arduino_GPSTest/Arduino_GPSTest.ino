ec246d19316c2ed95c0d62f5485150198e969413
gordonpipa <gpipa@uos.de>
2023-03-21 19:02:32 -0700

    Update Arduino_GPSTest.ino

--- V2/Arduino_GPSTest/Arduino_GPSTest.ino
+++ V2/Arduino_GPSTest/Arduino_GPSTest.ino
@@ -6,11 +6,14 @@
 #define SerialAT Serial1
 
 #define TINY_GSM_MODEM_SIM7000
+#define TINY_GSM_DEBUG SerialMon
 #define TINY_GSM_RX_BUFFER 1024  // Set RX buffer to 1Kb
 #define SerialAT Serial1
 #define DebbugFlag 1
 #define GPSdelay 1000
 
+#define TINY_GSM_USE_GPRS false
+#define TINY_GSM_USE_WIFI true
 // See all AT commands, if wanted
 // #define DUMP_AT_COMMANDS
 
@@ -22,15 +25,29 @@ const char apn[] = "YOUR-APN";  //SET TO YOUR APN
 const char gprsUser[] = "";
 const char gprsPass[] = "";
 
+const char* wifiSSID = "1495_Guest";
+const char*  wifiPass = "14951495";
+
+
+
+
+// MQTT details
+const char* broker = "broker.hivemq.com";
+
+const char* topicLed = "GsmClientTest/led";
+const char* topicInit = "GsmClientTest/init";
+const char* topicLedStatus = "GsmClientTest/ledStatus";
+
+
 #include <TinyGsmClient.h>
+#include <PubSubClient.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Ticker.h>
 #include <time.h>
 #include <WiFi.h>
 
-const char* ssid = "1495_Guest";
-const char* password = "14951495";
+
 long timezone = 1;
 byte daysavetime = 1;
 String dataMessage;
@@ -43,6 +60,9 @@ TinyGsm modem(debugger);
 #else
 TinyGsm modem(SerialAT);
 #endif
+
+
+
 
 #define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
 #define TIME_TO_SLEEP 60           // Time ESP32 will go to sleep (in seconds)
@@ -59,6 +79,26 @@ TinyGsm modem(SerialAT);
 #define SD_CS 13
 #define LED_PIN 12
 
+// Just in case someone defined the wrong thing..
+#if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
+#undef TINY_GSM_USE_GPRS
+#undef TINY_GSM_USE_WIFI
+#define TINY_GSM_USE_GPRS false
+#define TINY_GSM_USE_WIFI true
+#endif
+#if TINY_GSM_USE_WIFI && not defined TINY_GSM_MODEM_HAS_WIFI
+#undef TINY_GSM_USE_GPRS
+#undef TINY_GSM_USE_WIFI
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+#endif
+
+TinyGsmClient client(modem);
+PubSubClient mqtt(client);
+
+
+
+Ticker tick;
 
 void enableGPS(void) {
   // Set SIM7000G GPIO4 LOW ,turn on GPS power
@@ -153,6 +193,49 @@ void modemRestart() {
   modemPowerOn();
 }
 
+
+int ledStatus = LOW;
+uint32_t lastReconnectAttempt = 0;
+
+void mqttCallback(char* topic, byte* payload, unsigned int len) {
+
+  SerialMon.print("Message arrived [");
+  SerialMon.print(topic);
+  SerialMon.print("]: ");
+  SerialMon.write(payload, len);
+  SerialMon.println();
+
+  // Only proceed if incoming message's topic matches
+  if (String(topic) == topicLed) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_PIN, ledStatus);
+    SerialMon.print("ledStatus:");
+    SerialMon.println(ledStatus);
+    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+  }
+}
+
+boolean mqttConnect() {
+  SerialMon.print("Connecting to ");
+  SerialMon.print(broker);
+
+  // Connect to MQTT Broker
+  boolean status = mqtt.connect("GsmClientTest");
+
+  // Or, if you want to authenticate MQTT:
+  // boolean status = mqtt.connect("GsmClientName", "mqtt_user", "mqtt_pass");
+
+  if (status == false) {
+    SerialMon.println(" fail");
+    return false;
+  }
+  SerialMon.println(" success");
+  mqtt.publish(topicInit, "GsmClientTest started");
+  mqtt.subscribe(topicLed);
+  return mqtt.connected();
+}
+
+
 void setup() {
   // Set console baud rate
   SerialMon.begin(115200);
@@ -192,9 +275,96 @@ void setup() {
   Serial.println("antenna has been connected to the GPS port on the board.");
   Serial.println("/**********************************************************/\n\n");
   Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
+
+
+
+
+  Serial.println("\nWait...");
+
+  delay(1000);
+
+  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println("Initializing modem...");
+  if (!modem.restart()) {
+    Serial.println("Failed to restart modem, attempting to continue without restarting");
+  }
+
+
+
+  String name = modem.getModemName();
+  DBG("Modem Name:", name);
+
+  String modemInfo = modem.getModemInfo();
+  DBG("Modem Info:", modemInfo);
+
+#if TINY_GSM_USE_GPRS
+  // Unlock your SIM card with a PIN if needed
+  if (GSM_PIN && modem.getSimStatus() != 3) {
+    modem.simUnlock(GSM_PIN);
+  }
+#endif
+
+#if TINY_GSM_USE_WIFI
+  // Wifi connection parameters must be set before waiting for the network
+  SerialMon.print(F("Setting SSID/password..."));
+  if (!modem.networkConnect(wifiSSID, wifiPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+#endif
+
+#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
+  // The XBee must run the gprsConnect function BEFORE waiting for network!
+  modem.gprsConnect(apn, gprsUser, gprsPass);
+#endif
+
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+#if TINY_GSM_USE_GPRS
+  // GPRS connection parameters are usually set after network registration
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS connected");
+  }
+#endif
+
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
+
+
+
+
+
+
+
+
+  // Serial.print("Connecting to ");
+  // Serial.println(ssid);
+  // WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -203,6 +373,9 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println("Contacting Time Server");
+
+
+
 
 
   delay(10000);
@@ -332,7 +505,7 @@ void loop() {
         Bat_mv = readBattery();
         Sol_mv = readSolar();
 
-        dataMessage = String(i) + "," + String(Bat_mv) + ","  + String(sec + min * 60 + hour * 60 * 60) + "," + String(Act_time_ms) + "," + String(Runtime_ms) + "," + String(speed, 4) + "," + String(lat, 8) + "," + String(lon, 8) + ","  + String(accuracy) + "\r\n";
+        dataMessage = String(i) + "," + String(Bat_mv) + "," + String(sec + min * 60 + hour * 60 * 60) + "," + String(Act_time_ms) + "," + String(Runtime_ms) + "," + String(speed, 4) + "," + String(lat, 8) + "," + String(lon, 8) + "," + String(accuracy) + "\r\n";
         appendFile(SD, filename.c_str(), dataMessage.c_str());
         Last_loop_Act_time_ms = Act_time_ms;
         Act_time_ms = (esp_timer_get_time() - TimeTickerStart_us) / 1000;
